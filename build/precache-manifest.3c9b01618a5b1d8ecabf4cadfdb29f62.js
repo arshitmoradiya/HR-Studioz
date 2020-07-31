36357697cf6da6ed8fcc120e301e79e8f16be1c6
arshit moradiya <arshitmoradiya007@gmail.com>
2020-07-31 14:45:11 +0530

    project

---
+++ build/precache-manifest.3c9b01618a5b1d8ecabf4cadfdb29f62.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ff668269f55d539c407",
+    "url": "/HR_Studioz/static/css/main.9cd39f70.chunk.css"
+  },
+  {
+    "revision": "2ff668269f55d539c407",
+    "url": "/HR_Studioz/static/js/main.eb1b798d.chunk.js"
+  },
+  {
+    "revision": "5ada53b1e9cb2df58652",
+    "url": "/HR_Studioz/static/js/runtime~main.3f099985.js"
+  },
+  {
+    "revision": "bc749dfb7026f3c90481",
+    "url": "/HR_Studioz/static/css/2.25433cc5.chunk.css"
+  },
+  {
+    "revision": "bc749dfb7026f3c90481",
+    "url": "/HR_Studioz/static/js/2.49801a44.chunk.js"
+  },
+  {
+    "revision": "c0712855d6af1e0b9dd23412526866bc",
+    "url": "/HR_Studioz/static/media/logo.c0712855.PNG"
+  },
+  {
+    "revision": "7e7851537a10aec6e0c8b40c8eefde6a",
+    "url": "/HR_Studioz/static/media/harshil.7e785153.jpg"
+  },
+  {
+    "revision": "97075bb0e6fe319d4045369059ba0479",
+    "url": "/HR_Studioz/static/media/IMG_8214.97075bb0.JPG"
+  },
+  {
+    "revision": "3f48f49db6ab206da9f3f4d3717b95d0",
+    "url": "/HR_Studioz/static/media/IMG_9986.3f48f49d.jpg"
+  },
+  {
+    "revision": "c1dd56fa7ed7ef46ec077e1451ed6796",
+    "url": "/HR_Studioz/static/media/IMG_FILM.c1dd56fa.JPG"
+  },
+  {
+    "revision": "7c0597134d02810e9d60243b10abcb0c",
+    "url": "/HR_Studioz/static/media/IMG.7c059713.jpg"
+  },
+  {
+    "revision": "42565392c31f841d7f1df7ac50ff6c6e",
+    "url": "/HR_Studioz/static/media/IMG_9988.42565392.JPG"
+  },
+  {
+    "revision": "50c3d8a319ec7a9a115f2a74a7fe57ac",
+    "url": "/HR_Studioz/static/media/IMG_9987.50c3d8a3.JPG"
+  },
+  {
+    "revision": "69ee1094da0006e0166f6739ffb13fc8",
+    "url": "/HR_Studioz/static/media/IMG_0663.69ee1094.PNG"
+  },
+  {
+    "revision": "da6da0fb3adbe48c561cabfa43e2b3e3",
+    "url": "/HR_Studioz/static/media/1.da6da0fb.jpg"
+  },
+  {
+    "revision": "bae3b16c4696887ac5b6bd3dcc960a1a",
+    "url": "/HR_Studioz/static/media/2.bae3b16c.jpg"
+  },
+  {
+    "revision": "a2c11626b818b58bd1e9acd22c0d0ae2",
+    "url": "/HR_Studioz/index.html"
+  }
+];
